584e37e035b2e23e6966591bf928684964696ab6
informaticaefusach <informaticaefusach@usach.cl>
2026-03-24 09:40:16 -0300

    v2

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +14,17 @@
     <!-- DataTables Bootstrap 5 CSS -->
     <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="assets/styles.css" rel="stylesheet">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BDRRJGYKWH"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'G-BDRRJGYKWH');
+    </script>
+
 </head>
+
 <body>
-<div id="wrapper">
+    <div id="wrapper">
