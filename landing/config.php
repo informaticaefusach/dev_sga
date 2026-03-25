8fcc761a51d7deaecf8b3c657ddfdf7ca2382647
informaticaefusach <informaticaefusach@usach.cl>
2026-03-25 09:10:49 -0300

    v6

--- landing/config.php
+++ landing/config.php
@@ -1,24 +1,3 @@
 <?php
 
-$host = "localhost";
-$db   = "dev_cursos";
-$user = "dev";
-$pass = "Usach.-2025";
-
-try {
-
-    $pdo = new PDO(
-        "mysql:host=$host;dbname=$db;charset=utf8",
-        $user,
-        $pass
-    );
-
-
-
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-} catch (PDOException $e) {
-
-    die("Error de conexión: " . $e->getMessage());
-
-}
+require_once __DIR__ . '/../db.php';
